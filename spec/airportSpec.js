a7f0f0e391567c9fc18144639882ae64f58cd7f3
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-22 23:59:13 +0100

    Spy for plane in spec

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,43 +1,43 @@
 describe("Airport", function (){
   var airport;
+  var plane;
 
 
   beforeEach(function() {
     airport = new Airport();
+    plane = jasmine.createSpy(plane)
     airport.stormy = false
-
-});
+ });
   it("should have an empty array of planes at the start", function(){
     expect(airport.planes).toEqual([]);
   })
 
   it("should let a plane land", function(){
-    var plane = "RAM"
     airport.land(plane)
     console.log(airport.land(plane));
-    expect(airport.planes).toContain("RAM")
+    expect(airport.planes).toContain(plane)
   })
 
   it("should let a plane take off", function(){
-    var plane = "KLM"
     airport.land(plane)
     airport.takeOff(plane)
-    expect(airport.planes).not.toContain("KLM")
+    expect(airport.planes).not.toContain(plane)
   })
   it("lets you know which plane has taken off", function(){
-    var plane = "KLM"
     airport.land(plane)
-    expect(airport.takeOff(plane)).toEqual("KLM");
+    expect(airport.takeOff(plane)).toEqual(plane);
   })
 
 })
 
   describe("Stormy weather", function(){
     var airport;
+    var plane;
 
     beforeEach(function(){
       airport = new Airport();
-      airport.stormy = true
+      plane = jasmine.createSpy(plane)
+      airport.stormy = true;
     })
   it("doesn't let plane take off if stormy", function(){
     airport.planes = ["Monarch"]
@@ -45,11 +45,9 @@ describe("Airport", function (){
     expect(airport.planes).toContain("Monarch");
   })
   it("doesn't let plane land if strormy", function(){
-    var plane = "Meppel"
     expect(airport.land(plane)).toEqual("Weather too stormy to land")
   })
   it("doesn't let plane take off if no planes", function(){
-    var plane = "KazaK"
     expect(airport.takeOff(plane)).toEqual("No planes to take off");
 
   })
