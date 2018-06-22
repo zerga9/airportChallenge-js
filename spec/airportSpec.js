0a22d4ce8b143a5beaf6656820b0d485dbd2aca8
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-22 23:24:45 +0100

    Refactor specs with beforEach

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -4,8 +4,9 @@ describe("Airport", function (){
 
   beforeEach(function() {
     airport = new Airport();
-  });
+    airport.stormy = false
 
+});
   it("should have an empty array of planes at the start", function(){
     expect(airport.planes).toEqual([]);
   })
@@ -13,28 +14,38 @@ describe("Airport", function (){
   it("should let a plane land", function(){
     var plane = "RAM"
     airport.land(plane)
+    console.log(airport.land(plane));
     expect(airport.planes).toContain("RAM")
   })
 
   it("should let a plane take off", function(){
     var plane = "KLM"
     airport.land(plane)
-    airport.stormy = false
     airport.takeOff(plane)
     expect(airport.planes).not.toContain("KLM")
   })
   it("lets you know which plane has taken off", function(){
     var plane = "KLM"
     airport.land(plane)
-    airport.stormy = false
-    expect(airport.takeOff(plane)).toEqual("KLM")
+    expect(airport.takeOff(plane)).toEqual("KLM");
   })
+
+})
+
+  describe("Stormy weather", function(){
+    var airport;
+
+    beforeEach(function(){
+      airport = new Airport();
+      airport.stormy = true
+    })
   it("doesn't let plane take off if stormy", function(){
-    var plane = "Monarch"
-    airport.land(plane)
-    console.log(airport.stormy());
-    airport.stormy = true
-    airport.takeOff(plane)
-    expect(airport.planes).toContain("Monarch")
+    airport.planes = ["Monarch"]
+    airport.takeOff("Monarch")
+    expect(airport.planes).toContain("Monarch");
+  })
+  it("doesn't let plane land if strormy", function(){
+    var plane = "Meppel"
+    expect(airport.land(plane)).toEqual("Weather too stormy to land")
   })
 })
--- src/airport.js
+++ src/airport.js
@@ -3,16 +3,20 @@ function Airport(){
 };
 
 Airport.prototype.land = function(plane){
+  if (this.stormy){
+    return "Weather too stormy to land"
+  }else {
   this.planes.push(plane)
+}
 }
 
 Airport.prototype.takeOff = function(plane){
-  if (!this.stormy){
-   this.planes = this.planes.filter(item => item !== plane)
-   return plane
-} else {
-  return "Weather too stormy to take off!"
-}
+  if (this.stormy){
+    return "Weather too stormy to take off!"
+  } else {
+    this.planes = this.planes.filter(item => item !== plane)
+    return plane
+  }
 }
 
 Airport.prototype.stormy = function(){
