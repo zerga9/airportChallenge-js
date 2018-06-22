672be329c854a6d32eae66fd3f2bea44f29763f9
Gadiza Zerari <gadizaz@hotmail.com>
2018-06-22 13:00:41 +0100

    Test if Airport stormy no take off passes

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -19,12 +19,21 @@ describe("Airport", function (){
   it("should let a plane take off", function(){
     var plane = "KLM"
     airport.land(plane)
+    airport.stormy = false
     airport.takeOff(plane)
     expect(airport.planes).not.toContain("KLM")
   })
   it("lets you know which plane has taken off", function(){
     var plane = "KLM"
     airport.land(plane)
+    airport.stormy = false
     expect(airport.takeOff(plane)).toEqual("KLM")
+  })
+  it("doesn't let plane take off if stormy", function(){
+    var plane = "Monarch"
+    airport.land(plane)
+    airport.stormy = true
+    airport.takeOff(plane)
+    expect(airport.planes).toContain("Monarch")
   })
 })
--- src/airport.js
+++ src/airport.js
@@ -7,6 +7,17 @@ Airport.prototype.land = function(plane){
 }
 
 Airport.prototype.takeOff = function(plane){
+  if (!this.stormy){
    this.planes = this.planes.filter(item => item !== plane)
    return plane
+} else {
+  return "Weather too stormy to take off!"
+}
+}
+
+Airport.prototype.stormy = function(){
+  if (_.sample(['Stormy', 'Clear', 'Clear', 'Clear']) === 'Stormy'){
+    return true
+  }
+
 }
